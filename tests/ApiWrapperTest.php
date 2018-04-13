2f88adb9fdc33885f7f6e367d6f6165307cbdb10
Anam Hossain <anam.hossain@carsguide.com.au>
2018-04-13 15:28:52 +1000

    Should add query to request options [Test] #1

--- tests/ApiWrapperTest.php
+++ tests/ApiWrapperTest.php
@@ -142,6 +142,20 @@ class ApiWrapperTest extends TestCase
     }
 
     /**
+     * @test
+     */
+    public function shouldAddQueryToRequestOptions()
+    {
+        $api = new ApiWrapper($this->client);
+
+        $api->setQueryParams(['foo' => 'bar']);
+
+        $this->invokeMethod($api, 'setRequestOptions');
+
+        $this->assertCount(1, $api->requestOptions['query']);
+    }
+
+    /**
      *@test
      *
      */
@@ -222,6 +236,24 @@ class ApiWrapperTest extends TestCase
             ->setRequestType('GET')
             ->setResource('/vehicles/nvic')
             ->makeRequest();
+    }
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
     }
 
     /**
